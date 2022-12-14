e15676fadff3e6c4e788bc3c73d3f09303101d3f
Siebe <r0834360@student.thomasmore.be>
2022-12-14 18:30:28 +0100

    feat: add card display with logo donutello

--- Project/scripts/configurator.js
+++ Project/scripts/configurator.js
@@ -19,7 +19,7 @@ camera.position.z = 5; // <- New code
 const renderer = new THREE.WebGLRenderer({
     antialias: true
 });
-renderer.setClearColor("#FFFAFA");
+renderer.setClearColor("grey");
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -37,8 +37,11 @@ const light = new THREE.PointLight(0xFFFFFF, 1, 200);
 light.position.set(5, 5, 5);
 scene.add(light);
 
+
+
+
 //ambient light
-const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.1);
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.4);
 scene.add(ambientLight);
 
 
@@ -47,8 +50,8 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 controls.screenSpacePanning = false;
-controls.minDistance = 1;
-controls.maxDistance = 5;
+controls.minDistance = 3;
+controls.maxDistance = 6;
 
 
 camera.position.set(0, 20, 100);
@@ -61,105 +64,98 @@ controls.update();
 
 //get data-color from clicked div by class
 
-const buttons = document.querySelectorAll('.color-circle');
-buttons.forEach(button => {
-    button.addEventListener('click', function () {
-        console.log("button clicked");
-        let color = button.getAttribute("data-color");
-        console.log(color);
 
 
+//omport donu tmodel from obj file
+const loader = new OBJLoader();
+loader.load(
+    // resource URL
+    '/assets/models/Dough.obj',
+    // called when resource is loaded
+    function (object) {
 
-        console.log(button);
+        //add the color from the button when clicking button green
+        object.traverse(function (child) {
+            if (child.isMesh) {
+                //add the color
+                child.material.color.set("#F1B166");
 
-        //omport donu tmodel from obj file
-        const loader = new OBJLoader();
-        loader.load(
-            // resource URL
-            '/assets/models/Dough.obj',
-            // called when resource is loaded
-            function (object) {
-
-                //add the color from the button when clicking button green
-                object.traverse(function (child) {
-                    if (child.isMesh) {
-                        //add the color from the button when clicking button green
-                        child.material.color.set(color);
-                    }
-                });
-
-
-//object rotation set
-object.rotation.set(0.6, 5, 0.3);
-
-                object.scale.set(15, 15, 15);
-                scene.add(object);
-            },
-            // called when loading is in progresses
-            function (xhr) {
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-                document.getElementById("loading__time").innerHTML = (xhr.loaded / xhr.total * 100) + '% loaded';
-            },
-            // called when loading has errors
-            function (error) {
-                console.log('An error happened');
             }
-        )
+        });
 
-    });
 
-});
+        //object rotation set
+        object.rotation.set(0.6, 5, 0.3);
+
+        object.scale.set(15, 15, 15);
+        scene.add(object);
+    },
+    // called when loading is in progresses
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+        document.getElementById("loading__time").innerHTML = (xhr.loaded / xhr.total * 100) + '% loaded';
+    },
+    // called when loading has errors
+    function (error) {
+        console.log('An error happened');
+    }
+)
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-const buttonsglaze = document.querySelectorAll('.color-circle-glaze');
-buttonsglaze.forEach(buttonglaze => {
-    buttonglaze.addEventListener('click', function () {
-        console.log("button clicked");
-        let color = buttonglaze.getAttribute("data-color");
-        console.log(color);
 
 
-        //omport donu tmodel from obj file
-        const loader = new OBJLoader();
-        loader.load(
-            // resource URL
-            '/assets/models/Glaze.obj',
-            // called when resource is loaded
-            function (object) {
 
+//omport donu tmodel from obj file
+const loaderOne = new OBJLoader();
+loaderOne.load(
+    // resource URL
+    '/assets/models/Glaze.obj',
+    // called when resource is loaded
+    function (object) {
+
+        //add the color from the button when clicking button green
+        object.traverse(function (child) {
+            if (child.isMesh) {
                 //add the color from the button when clicking button green
-                object.traverse(function (child) {
-                    if (child.isMesh) {
-                        //add the color from the button when clicking button green
+
+                const buttonsglaze = document.querySelectorAll('.color-circle-glaze');
+                buttonsglaze.forEach(buttonglaze => {
+                    buttonglaze.addEventListener('click', function () {
+                        console.log("button clicked");
+                        let color = buttonglaze.getAttribute("data-color");
+                        console.log(color);
+
                         child.material.color.set(color);
-                    }
+
+                    })
                 });
 
-
-//object rotation set
-object.rotation.set(0.6, 5, 0.3);
-
-                object.scale.set(15, 15, 15);
-                scene.add(object);
-            },
-            // called when loading is in progresses
-            function (xhr) {
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-                document.getElementById("loading__time").innerHTML = (xhr.loaded / xhr.total * 100) + '% loaded';
-            },
-            // called when loading has errors
-            function (error) {
-                console.log('An error happened');
             }
-        )
+        });
 
-    });
 
-});
+        //object rotation set
+        object.rotation.set(0.6, 5, 0.3);
+
+        object.scale.set(15, 15, 15);
+        scene.add(object);
+    },
+    // called when loading is in progresses
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+        document.getElementById("loading__time").innerHTML = (xhr.loaded / xhr.total * 100) + '% loaded';
+    },
+    // called when loading has errors
+    function (error) {
+        console.log('An error happened');
+    }
+)
+
+
 
 //when clicking button reload obj file
 
@@ -185,14 +181,14 @@ buttonssprinkles.forEach(buttonsprinkle => {
                 //add the color from the button when clicking button green
                 object.traverse(function (child) {
                     if (child.isMesh) {
-                        //add the color from the button when clicking button green
-                        child.material.color.set(color);
+                        //add texture to the object
+
                     }
                 });
 
 
-//object rotation set
-object.rotation.set(0.6, 5, 0.3);
+                //object rotation set
+                object.rotation.set(0.6, 5, 0.3);
 
                 object.scale.set(15, 15, 15);
                 scene.add(object);
@@ -214,7 +210,34 @@ object.rotation.set(0.6, 5, 0.3);
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
-//buttincard from id
+
+//load object card.obj
+
+//aad litlle cube on glaze object
+const geometry = new THREE.BoxGeometry(1, 0.05, 1);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+cube.position.set(0.3, 0.5, 1.5);
+cube.rotation.set(0.6, 0,0);
+
+//add texture to little cube
+const texture = new THREE.TextureLoader().load('/assets/textures/Donutello_card.jpg');
+const materialTexture = new THREE.MeshBasicMaterial({
+    map: texture
+});
+//texture rotation in degrees
+
+cube.material = materialTexture;
+
+
+
+
+
+
 
 
 
@@ -231,9 +254,9 @@ const rendering = function () {
 
 
 
-    
-    
- 
+
+
+
     renderer.render(scene, camera);
 }
 rendering();
