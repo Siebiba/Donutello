2ced1ca92403e82019ce5224b9d4f90478ff25f4
Siebe <r0834360@student.thomasmore.be>
2022-12-14 08:33:34 +0100

    feat: add threejs scene and camera

--- Project/scripts/configurator.js
+++ Project/scripts/configurator.js
@@ -1,1 +1,18 @@
+//import threejs
 
+import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+
+//Scene
+const scene = new THREE.Scene();
+//Camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5; // <- New code
+//Renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#233143");
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
